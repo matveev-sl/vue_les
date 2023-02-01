29006d56317547f7ce287a9152bbdbcc66431091
Viacheslav Matveev <slava.matveev@gmail.com>
2023-02-01 03:09:09 +0300

    First Screen

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,93 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import NavigateMenu from './components/NavigateMenu.vue'
 </script>
 
 <template>
   <div id="app">
     <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+      <nav class="menu">
+    <img class="logo" src="@/assets/images/Logo.svg">
+    <ul class="nav">
+        <li >Home</li>
+        <li >Home</li>
+        <li >Home</li>
+        <li >Home</li>
+        <li >Home</li>
+        <li >Home</li>
 
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-
-        <nav>
-          <router-link to="/">Home</router-link>
-          <router-link to="/about">About</router-link>
-        </nav>
-      </div>
+    </ul>
+  </nav>
     </header>
+<section class="firstScreen">
+<div class="Title">
+<img class="BigLogo" src="@/assets/images/BigLogo.svg">
+<img class="Line" src="@/assets/images/Line 3.svg">
+<p class="AfterLine">User Interface Designer</p>
+<p class="Subline">I’m UI/UX designer in lucknow , and I’m  very passionate 
+and dedicated to my work..</p>
+<button class="SayHello">say Hello <img class="Arrow" src="@/assets/images/Vector.svg"> </button>
+</div>
+<img class="avatar" src="@/assets/images/Avatar.png">
+</section>
 
-    <router-view />
   </div>
 </template>
 
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.nav {
+  display: flex;
 }
+.nav li {
 
-.logo {
+font-style: normal;
+font-weight: 700;
+font-size: 7.62468px;
+line-height: 10px;
+list-style-type: none;
+margin-right: 20.63px;
+}
+.menu {
+  display: flex;
+  justify-content: space-between;
+}
+.BigLogo {
   display: block;
-  margin: 0 auto 2rem;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.AfterLine {
+margin-top: 10px;
+font-size: 8px;
+font-weight: 500;
+line-height: 10px;
+letter-spacing: 0em;
+text-align: left;
+color: #000000;
+display: inline-block;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.SayHello  {
+margin-top: 30px;
+background: #000000;
+border-radius: 5.38213px;
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 4.48511px 12.1098px;
+gap: 7.62px;
+color: #ffffff;
+font-size: 7.62468px;
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.Subline {
+  width: 70%;
 }
+/* .Title {
+  display: flex;
+}  */
+.firstScreen {
+  margin-top: 56px ;
+  display: flex;
+  justify-content: space-between;
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
